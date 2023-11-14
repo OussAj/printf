f09ee1f05e480aba5b18604f9f2114f4d484dd10
khalilouah97 <khalil.ouahmide@gmail.com>
2023-11-14 22:04:17 +0100

    betty kh

--- _strlen.c
+++ _strlen.c
@@ -4,16 +4,16 @@
 /**
  * _strlen - counts string length.
  * @str: the string to be operated.
- * 
+ *
  * Return: the length of string.
-*/
+ */
 int _strlen(char *str)
 {
-    int len = 0;
-    
-    while (str[len] != '\n')
-    {
-        len++;
-    }
-    return (len);
+	int len = 0;
+
+	while (str[len] != '\n')
+	{
+		len++;
+	}
+	return (len);
 }
